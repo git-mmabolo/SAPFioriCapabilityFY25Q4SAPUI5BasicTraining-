80455c91585c33083c4a42bcd220bcbb39f7ef0d
Mark Bryan Mabolo <mark.bryan.mabolo@accenture.com>
2025-08-06 15:14:57 +0000

    exercise1

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -4,7 +4,25 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("project2.controller.View2", {
-        onInit() {
+        onClearInput: function() {
+            const oInput1 = this.getView().byId("namInput1");
+            const oInput2 = this.getView().byId("namInput2");
+            const oInput3 = this.getView().byId("namInput3");
+            const oInput4 = this.getView().byId("namInput4");
+            const oInput5 = this.getView().byId("namInput5");
+            const oInput6 = this.getView().byId("select1");
+            oInput1.setValue("");
+            oInput2.setValue("");
+            oInput3.setValue("");
+            oInput4.setValue("");
+            oInput5.setValue("");
+            oInput6.setValue("");
+            MesageToast.show("Input Field Cleared!");
         }
+/*        
+        onInit() {
+           
+        }
+*/        
     });
 });
